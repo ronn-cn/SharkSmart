14f7a982998f181758742c593382d6437597851b
yinchongqiang <775709948@qq.com>
2020-03-25 10:03:38 +0800

    修改资源

--- SharkSmart/FrmEditor.cs
+++ SharkSmart/FrmEditor.cs
@@ -7,6 +7,9 @@ namespace SharkSmart
 {
     public partial class FrmEditor : Formium
     {
+
+        //public override string StartUrl => "";
+
         private string filePath;
         public string FilePath
         {
@@ -17,22 +20,6 @@ namespace SharkSmart
                 if (!string.IsNullOrEmpty(filePath))
                     content = File.ReadAllText(filePath).Replace("\n", "\\n").Replace("\r", "\\r");
                 ExecuteJavascript("setValue(\"" + content + "\")");
-
-                //string modName = Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(filePath)));
-
-                //string str = "";
-
-                //ExecModule mod = Engine.Project.GetModule(modName) as ExecModule;
-                //UIUnit ut = mod.GetUIUnit();
-                //if (ut != null)
-                //{
-                //    foreach (var page in ut.Pages)
-                //    {
-                //        foreach (var ctl in page.Controls)
-                //            str += ctl.Name + "|";
-                //    }
-                //}
-                //ExecuteJavascript("setTips(\"" + str + "\")");
             }
             get
             {
@@ -42,7 +29,7 @@ namespace SharkSmart
 
         //public event EventHandler UpdateEvent;
 
-        public FrmEditor() : base(Environment.CurrentDirectory + @"\editor\editor.html")
+        public FrmEditor() : base(Environment.CurrentDirectory + @"\editor\min\editor.html")
         {
             InitializeComponent();
             var fun = GlobalObject.AddFunction("SaveFile");
